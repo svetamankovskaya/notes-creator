8971710ad8a6fcbb9cacdb87af449514ec968a96
Sveta <kazhurosveta@mail.ru>
2019-06-18 14:33:23 +0300

    added header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import classes from './App.module.css';
 import NoteEditorCreator from './components/NoteEditorCreator/NoteEditorCreator';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className={classes.appWrapper}>
-      <header className={classes.appHeader}>
-          Заметки
-      </header>
+      <Header />
       <div className={classes.nav}>
         <div>Поиск</div>
         <div>Заметки список всех</div>
