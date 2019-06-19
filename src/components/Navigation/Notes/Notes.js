30b25dba869bf9ce3b7d443d357e38a60cb4845a
Sveta <kazhurosveta@mail.ru>
2019-06-19 16:19:54 +0300

    updated note component

--- src/components/Navigation/Notes/Notes.js
+++ src/components/Navigation/Notes/Notes.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import classes from './Notes.module.css';
 
+let Note = (props) => {
+    return <div className={classes.noteWrapper}>
+        <div>{props.note.text}</div>
+        <div>{props.note.title}</div>
+    </div>
+}
+
 let Notes = (props) => {
-    let notesElements = props.notes.map(note => <div key={note.id}>{note.text}</div>)
+    let notesElements = props.notes.map(note => <Note key={note.id} note={note}/>)
     return <div>
         {notesElements}
     </div>
