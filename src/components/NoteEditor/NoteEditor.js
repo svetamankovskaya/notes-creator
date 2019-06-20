ccc273c67a1c8a7536e2326793c056075934f4a9
Sveta <kazhurosveta@mail.ru>
2019-06-20 11:54:43 +0300

    styled button "Новая заметка"

--- src/components/NoteEditor/NoteEditor.js
+++ src/components/NoteEditor/NoteEditor.js
@@ -4,13 +4,17 @@ import classes from './NoteEditor.module.css';
 const NoteEditor = (props) => {
   return (
     <div className={classes.noteEditor}>
-      <div>Кнопка "создать заметку"</div>
-      <div className={classes.noteWrapper}>
-      <div className={classes.noteTitle}>{props.note.title}</div> 
-      <div className={classes.noteText}>{props.note.text}</div>
-      <div className={classes.hashtags}>Hashtags</div>
+      <div>
+        <button className={classes.newNoteButton}>
+          <span>Новая заметка</span>
+        </button>
       </div>
-      
+      <div className={classes.noteWrapper}>
+        <div className={classes.noteTitle}>{props.note.title}</div>
+        <div className={classes.noteText}>{props.note.text}</div>
+        <div className={classes.hashtags}>Hashtags</div>
+      </div>
+
     </div>
   )
 }
